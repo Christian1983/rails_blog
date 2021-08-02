39800f97c1f81aee8b5a6cfd5e9e0b46d8c0dfbf
Christian Neumann <psychedelic.system@gmail.com>
2021-08-02 20:07:17 +0200

    home view done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :set_data
 
     def set_data
+        @last_posts  = Post.order(ref_date: :desc).limit(3)
         @month_group = Post.month_group_count
     end
 end
