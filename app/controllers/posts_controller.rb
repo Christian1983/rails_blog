e7aa95f202309e4f586199a3dc9df2f5c84aede4
Christian Neumann <psychedelic.system@gmail.com>
2021-08-02 23:50:52 +0200

    view changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    
+    @posts =  Post.all.map.with_object({}) do |post, hash|
+      hash[post.ref_date.strftime('%B')] ||= []
+      hash[post.ref_date.strftime('%B')] << post
+    end
+    
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
     def self.month_group_count
+        #TODO: nicht performant, group in datenbank lösen
         self.all.map {|post| post.ref_date.strftime('%B')}.tally
     end
 end
