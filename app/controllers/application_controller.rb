8faee165a1043675d6190ba31ef63bcd51db5285
Christian Neumann <psychedelic.system@gmail.com>
2021-08-02 19:51:16 +0200

    month view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :set_data
+
+    def set_data
+        @month_group = Post.month_group_count
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+    def self.month_group_count
+        self.all.map {|post| post.ref_date.strftime('%B')}.tally
+    end
 end
