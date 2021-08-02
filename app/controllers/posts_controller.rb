1b9c593c72081b79eb1ed2ca6c691b77d183e8ae
Christian Neumann <psychedelic.system@gmail.com>
2021-08-03 00:36:51 +0200

    css changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     
+    #TODO: nicht performant, group in datenbank lösen
     @posts =  Post.order(ref_date: :desc).map.with_object({}) do |post, hash|
       hash[post.ref_date.strftime('%B')] ||= []
       hash[post.ref_date.strftime('%B')] << post
